5e5eed60d5610e5d9ee8aa83dbe12bd722804c43
Taeyong Lee <d104601@hotmail.com>
2022-06-14 23:02:34 -0400

    App Interface

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO department (id, name)
+VALUES (1, "Web Development"),
+       (2, "Data Science");
+       
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, "Front End", 55000, 1),
+       (2, "Back End", 65000, 1),
+       (3, "Full Stack", 75000, 1),
+       (4, "Data Analyst", 80000, 2),
+       (5, "Developer", 70000, 2);
+       
--- index.js
+++ index.js
@@ -6,16 +6,63 @@ const db = mysql.createConnection(
     {
         host: "localhost",
         user: "root",
-        password: "",
-        database: "content_db"
+        password: "taeyong1992",
+        database: "company_db"
     },
-    console.log("Connected content_db")
+    console.log("Connected company_db")
 );
 
+function allEmployee() {
+    db.query("SELECT * FROM employee", (err, result) => {
+        if (err) {
+            throw err;
+        }
+        consoleTable.getTable(result);
+        main();
+    });
+}
 
+function addEmployee() {
+    db.query("")
+    main();
+}
 
-function init() {
-    console.log("Welcome to Employee Manager.")
+function allRoles() {
+    db.query("SELECT * FROM role", (err, result) => {
+        if (err) {
+            throw err;
+        }
+        consoleTable.getTable(result);
+    });
+    main();
+}
+
+function addRole() {
+
+    main();
+}
+
+function allDept() {
+    db.query("SELECT * FROM department", (err, result) => {
+        if (err) {
+            throw err;
+        }
+        consoleTable.getTable(result);
+    });
+    main();
+}
+
+function addDept() {
+
+    main();
+}
+
+function update() {
+
+    main();
+}
+
+function main() {
     inquirer.prompt([
         {
             type: "list",
@@ -28,8 +75,43 @@ function init() {
                 "Add Role",
                 "View All Departments",
                 "Add Department",
-                "Update Employee Role"
+                "Update Employee Role",
+                "Exit"
             ]
         }
-    ]).then()
+    ]).then((answer) => {
+        switch(answer.selection) {
+            case "View All Employees":
+                allEmployee();
+                break;
+            case "Add Employee":
+                addEmployee();
+                break;
+            case "View All Roles":
+                allRoles();
+                break;
+            case "Add Role":
+                addRole();
+                break;
+            case "View All Departments":
+                allDept();
+                break;
+            case "Add Department":
+                addDept();
+                break;
+            case "Update Employee Role":
+                update();
+                break;
+            default:
+                console.log("GoodBye");
+                process.exit();
+        }
+    });
 }
+
+function init() {
+    console.log("Welcome to Employee Manager.")
+    main();
+}
+
+init();
